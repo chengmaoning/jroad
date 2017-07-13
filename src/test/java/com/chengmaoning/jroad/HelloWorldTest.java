de374f67b5adfba6c4a9956ffe4bc78155351640
jackie.cn <jackie.cn@alibaba-inc.com>
2017-07-13 16:48:10 +0800

    modify pom and add test
    Signed-off-by: jackie.cn <jackie.cn@alibaba-inc.com>

---
+++ src/test/java/com/chengmaoning/jroad/HelloWorldTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.chengmaoning.jroad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * HelloWorldTest.java
+ * 
+ * @author chengmaoning
+ *
+ *         2017年7月13日下午4:38:16
+ */
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
