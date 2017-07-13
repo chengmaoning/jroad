59f232192171f2dff62f860805a5edd8ecb1af0e
jackie.cn <jackie.cn@alibaba-inc.com>
2017-07-13 15:39:58 +0800

    init
    Signed-off-by: jackie.cn <jackie.cn@alibaba-inc.com>

---
+++ src/main/java/com/chengmaoning/jroad/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.chengmaoning.jroad;
+
+/**
+ * 
+ * HelloWorld.java
+ * @author chengmaoning
+ *
+ * 2017年7月13日下午3:01:36
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+
+}
