d92b5abd9f89186334629fbddfcd7ad24a624e4d
chengmaoning <chengmaoning@sina.com>
2017-07-30 23:12:06 +0800

    jdbctemplate case

---
+++ src/test/java/com/chengmaoning/jroad/jdbc/JdbcStudentDaoTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.chengmaoning.jroad.jdbc;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author chengmaoning
+ *
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:application-context.xml")
+public class JdbcStudentDaoTest {
+
+	@Autowired
+	StudentDao studentDao;
+
+	/**
+	 * Test method for
+	 * {@link com.chengmaoning.jroad.jdbc.JdbcStudentDao#findAllStudents()}.
+	 */
+	@Test
+	public void testFindAllStudents() {
+
+		List<Student> students = studentDao.findAllStudents();
+		System.out.println(students);
+	}
+
+}
