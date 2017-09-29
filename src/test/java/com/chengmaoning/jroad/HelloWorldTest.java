9dcf3874446a3f848e1089e3877eadcef81ad351
chengmaoning <chengmaoning@sina.com>
2017-09-29 15:24:19 +0800

    re
    Signed-off-by: chengmaoning <chengmaoning@sina.com>

--- src/test/java/com/chengmaoning/jroad/HelloWorldTest.java
+++ src/test/java/com/chengmaoning/jroad/HelloWorldTest.java
@@ -24,16 +24,7 @@ public class HelloWorldTest {
 		System.out.println("inside a test.");
 		assertTrue(true);
 		
-		
-		List<String> expOrderIds = Arrays.asList("1");
-		
-		StringBuffer sb = new StringBuffer("(");
-        for(String order: expOrderIds){
-            sb.append("'").append(order).append("',");
-        }
-        sb.deleteCharAt(sb.length()-1).append(")");
-        
-        System.out.println(sb.toString());
+		String errMsg = "hello\nworld.";
 	}
 
 }
