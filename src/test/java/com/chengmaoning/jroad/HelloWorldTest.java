0871ac4e571667b515f497e9bcb646615f14801c
chengmaoning <chengmaoning@sina.com>
2017-10-09 00:31:58 +0800

    re
    Signed-off-by: chengmaoning <chengmaoning@sina.com>

--- src/test/java/com/chengmaoning/jroad/HelloWorldTest.java
+++ src/test/java/com/chengmaoning/jroad/HelloWorldTest.java
@@ -5,6 +5,8 @@ package com.chengmaoning.jroad;
 
 import static org.junit.Assert.*;
 
+import java.net.URL;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
@@ -28,6 +30,22 @@ public class HelloWorldTest {
 		logger.info("info...");
 		logger.warn("warn...");
 		logger.error("error...");
+		
+		ClassLoader c = getClass().getClassLoader();
+		logger.info(c);
+		ClassLoader c1 = c.getParent();
+		logger.info(c1);
+		ClassLoader c2 = c1.getParent();
+		logger.info(c2);
+		
+		@SuppressWarnings("restriction")
+		URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+		for (int i = 0; i < urls.length; i++) {
+			logger.info(urls[i].toExternalForm());
+		}
+		
+		logger.info(System.getProperty("sun.boot.class.path"));
+		
 	}
 
 }
