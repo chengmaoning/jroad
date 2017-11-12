c396d30051b5c499d18928041116024bb22b0b61
chengmaoning <chengmaoning@sina.com>
2017-11-12 22:26:25 +0800

    functional intefaces
    Signed-off-by: chengmaoning <chengmaoning@sina.com>

--- src/test/java/com/chengmaoning/jroad/HelloWorldTest.java
+++ src/test/java/com/chengmaoning/jroad/HelloWorldTest.java
@@ -6,6 +6,9 @@ package com.chengmaoning.jroad;
 import static org.junit.Assert.*;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -19,20 +22,20 @@ public class HelloWorldTest {
 	public void test() {
 		System.out.println("inside a test.");
 		assertTrue(true);
-		
+
 		String errMsg = "hello\nworld.";
-		
-		System.out.println( errMsg.indexOf("\n"));
-		
+
+		System.out.println(errMsg.indexOf("\n"));
+
 		System.out.println(errMsg.substring(0, errMsg.indexOf("\n")));
 
 		logger.debug("debug...");
 		logger.info("info...");
 		logger.warn("warn...");
 		logger.error("error...");
-		
+
 		System.out.println(System.getProperty("java.class.path"));
-		
+
 		System.out.println(System.getProperty("java.ext.dirs"));
 
 		ClassLoader c = getClass().getClassLoader();
@@ -41,15 +44,30 @@ public class HelloWorldTest {
 		logger.info(c1);
 		ClassLoader c2 = c1.getParent();
 		logger.info(c2);
-		
+
 		@SuppressWarnings("restriction")
 		URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
 		for (int i = 0; i < urls.length; i++) {
 			logger.info(urls[i].toExternalForm());
 		}
-		
+
 		logger.info(System.getProperty("sun.boot.class.path"));
-		
+
+	}
+
+	@Test
+	public void test_functoin() {
+		List<Integer> array = Arrays.asList(3, 25, 5, 2, 8, 9, 6, 10);
+
+		Predicate<Integer> predicate = n -> n % 2 == 0;
+
+		for (int i = 0; i < array.size(); i++) {
+			if (predicate.test(array.get(i))) {
+				logger.info(array.get(i) + ": even.");
+			} else {
+				logger.info(array.get(i) + ": odd.");
+			}
+		}
 	}
 
 }
