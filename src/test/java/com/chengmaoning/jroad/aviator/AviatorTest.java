544c987da0f4ddf2c8c93df83b448a9d156a9cf3
chengmaoning <chengmaoning@sina.com>
2019-06-04 16:18:00 +0800

    test
    Signed-off-by: chengmaoning <chengmaoning@sina.com>

--- src/test/java/com/chengmaoning/jroad/aviator/AviatorTest.java
+++ src/test/java/com/chengmaoning/jroad/aviator/AviatorTest.java
@@ -2,11 +2,21 @@ package com.chengmaoning.jroad.aviator;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.Random;
+import java.util.TreeMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -81,16 +91,65 @@ public class AviatorTest {
 
 	@Test
 	public void test4() {
-		TestBean testBean = new TestBean();
-		testBean.setKey("hello");
-		testBean.setValue("world");
+		String exp = "1_88c56276d6a5ffa101f2567b4adec11d and 1_d61f14f8982d0b855917f0b8c2655c96 and 1_7ce5135c2e5ca3704dfe457d18eed205";
+		String expression = exp.replace("and", "&&").replace("or", "||");
+		Map<String, Object> env = new HashMap<>();
+		env.put("payeeUserId", "2088331593939528");
+		env.put("bizProductCheck", "CITY_TRFC_QRCODE_PAY");
+		env.put("useCountLimit", "3");
 
-		String name = "dennis";
-		AviatorEvaluator.execute("#testBean.value = '666'", new HashMap() {
-			{
-				put("testBean", testBean);
+		Map<String, String> map = new HashMap<>();
+		map.put("1_88c56276d6a5ffa101f2567b4adec11d", StringUtils.EMPTY);
+		map.put("1_d61f14f8982d0b855917f0b8c2655c96", "string.contains(bizProductCheck,'CITY_TRFC_QRCODE_PAY')");
+		map.put("1_7ce5135c2e5ca3704dfe457d18eed205", "string.contains(payeeUserId,'2088331593939528')");
+
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			String key = entry.getKey();
+			String value = entry.getValue();
+			String ret = StringUtils.isBlank(value) ? "true"
+					: ((Boolean) AviatorEvaluator.execute(value, env)).toString();
+			expression = expression.replace(key, ret);
+		}
+		System.out.println("expression = " + expression);
+		boolean outcome = (boolean) AviatorEvaluator.execute(expression);
+		System.out.println("outcome = " + outcome);
+	}
+
+	@Test
+	public void test5() {
+		// String expression = "(true && true && true && true && true) || (true
+		// && true && true && true && true) || (false && true && true && true &&
+		// true && true) || (true && true && false && true && true && true) ||
+		// (true && false && true && true && true && true) || (true && true &&
+		// true && true && false && true) || (true && false && true && true &&
+		// true && true) || (true && true && true && false && true && true) ||
+		// (true && true && false && true && true && true) || (false && true &&
+		// true && true && true && true) || (false && true && true && true &&
+		// true && true) || (true && true && 1true && true && true && true) ||
+		// (true && true && true && false && true && true)";
+		// Boolean ret = (Boolean) AviatorEvaluator.execute(expression);
+		// System.out.println("ret = " + ret);
+	}
+
+	@Test
+	public void test6() {
+		Map<String, String> map = new HashMap<>();
+		map.put("8_e8ce7f738c8d97790b1ddd09da505d39", "444");
+		map.put("8_dbf2bf3eb73bd1ae48672d91a1759656", "333");
+		map.put("8_e8ce7f738c8d", "93493");
+
+		List<Map.Entry<String, String>> list = new ArrayList<>(map.entrySet());
+
+		Collections.sort(list, new Comparator<Map.Entry<String, String>>() {
+
+			@Override
+			public int compare(Entry<String, String> o1, Entry<String, String> o2) {
+				return o2.getKey().length() - o1.getKey().length();
 			}
 		});
-		System.out.println(JSON.toJSONString(testBean, true));
+
+		for (Map.Entry<String, String> entry : list) {
+			System.out.println("key = " + entry.getKey() + ", value = " + entry.getValue());
+		}
 	}
 }
